bf33a92c74c532ddb1b694e4f73aa68cce6d6059
Smoren <ofigate@gmail.com>
2024-12-15 03:23:11 +0300

    New release.

--- src/strategies.ts
+++ src/strategies.ts
@@ -7,11 +7,24 @@ import {
 } from "genetic-search";
 import { MultiprocessingMetricsStrategyConfig } from "./types";
 
+/**
+ * Base class for metrics strategies that uses multiprocessing to execute metrics calculation tasks.
+ *
+ * @template TGenome - The type of the genome.
+ * @template TConfig - The type of the configuration for the metrics strategy.
+ * @template TTaskConfig - The type of the configuration for each metrics calculation task.
+ */
 export abstract class BaseMultiprocessingMetricsStrategy<
   TGenome extends BaseGenome,
   TConfig extends MultiprocessingMetricsStrategyConfig<TTaskConfig>,
   TTaskConfig,
 > extends BaseMetricsStrategy<TGenome, TConfig, TTaskConfig> {
+  /**
+   * Execute the metrics calculation tasks.
+   *
+   * @param inputs The inputs to the metrics calculation tasks.
+   * @returns A matrix of metrics results, where each row corresponds to a single genome.
+   */
   protected async execTasks(inputs: TTaskConfig[]): Promise<GenerationMetricsMatrix> {
     const pool = new Pool(this.config.poolSize);
     const result: GenerationMetricsMatrix = await pool.map(inputs, this.config.task, {
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,12 @@
 import type { MetricsStrategyConfig } from "genetic-search";
 
+/**
+ * Configuration for the Multiprocessing Metrics Strategy.
+ *
+ * @template TTaskConfig The type of metrics task calculation configuration.
+ *
+ * @property {number} poolSize The size of the multiprocessing pool.
+ */
 export type MultiprocessingMetricsStrategyConfig<TTaskConfig> = MetricsStrategyConfig<TTaskConfig> & {
   poolSize: number;
-}
+};
